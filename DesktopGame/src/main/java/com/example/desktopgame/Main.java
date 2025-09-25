41546ba5f423470b4027f7ab7fd597246116f60c
DiegoAlonsoValenciano <125390412+DiegoAlonsoValenciano@users.noreply.github.com>
2025-09-25 19:34:37 +0200

    AndroidEngine

--- DesktopGame/src/main/java/com/example/desktopgame/Main.java
+++ DesktopGame/src/main/java/com/example/desktopgame/Main.java
@@ -1,6 +1,11 @@
 package com.example.desktopgame;
 
+import com.example.desktopengine.DesktopEngine;
+import com.example.gamelogic.GameLogic;
+
 public class Main {
+    DesktopEngine desktopEngine;
+    GameLogic gameLogic;
     public static void main(String[] args) {
 
     }
--- app/src/main/java/com/example/towerdefense/MainActivity.java
+++ app/src/main/java/com/example/towerdefense/MainActivity.java
@@ -8,8 +8,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.gamelogic.GameLogic;
 
+public class MainActivity extends AppCompatActivity {
+    GameLogic gameLogic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
