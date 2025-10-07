1134281940f80a5138d343b54116de3e15d37079
DiegoAlonsoValenciano <125390412+DiegoAlonsoValenciano@users.noreply.github.com>
2025-10-07 19:30:50 +0200

    input

--- Engine/src/main/java/com/example/engine/Input.java
+++ Engine/src/main/java/com/example/engine/Input.java
@@ -1,4 +1,8 @@
 package com.example.engine;
 
+import java.util.List;
+
 public interface Input {
+
+    public List<TouchEvent> getInput();
 }
---
+++ Engine/src/main/java/com/example/engine/TouchEvent.java
@@ -0,0 +1,8 @@
+package com.example.engine;
+
+public class TouchEvent {
+
+    public static enum TouchType{
+        
+    }
+}
