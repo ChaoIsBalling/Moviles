3db9460212d27fb71a26c6137c34ebb025989109
DiegoAlonsoValenciano <125390412+DiegoAlonsoValenciano@users.noreply.github.com>
2025-10-30 18:42:43 +0100

    torres

---
+++ GameLogic/src/main/java/com/example/gamelogic/FireTower.java
@@ -0,0 +1,37 @@
+package com.example.gamelogic;
+
+import com.example.engine.Graphics;
+
+import java.util.ArrayList;
+
+public class FireTower implements Tower {
+    @Override
+    public void UpdateAttack(float mejora) {
+
+    }
+
+    @Override
+    public void UpdateRange(float mejora) {
+
+    }
+
+    @Override
+    public void UpdateFireRate(float mejora) {
+
+    }
+
+    @Override
+    public void setListaEnemigos(ArrayList<Enemy> enemigos) {
+
+    }
+
+    @Override
+    public void Update(double deltaTime) {
+
+    }
+
+    @Override
+    public void Render(Graphics gr) {
+
+    }
+}
