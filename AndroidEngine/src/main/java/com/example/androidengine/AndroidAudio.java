50a8cb91f165891e5b061cef8378e899d0e0bc68
Chao <114423476+ChaoIsBalling@users.noreply.github.com>
2025-11-01 15:16:54 +0100

    Audio en android

--- AndroidEngine/src/main/java/com/example/androidengine/AndroidAudio.java
+++ AndroidEngine/src/main/java/com/example/androidengine/AndroidAudio.java
@@ -8,6 +8,8 @@ import com.example.engine.Audio;
 
 import java.io.IOException;
 
+import android.media.AudioAttributes;
+
 public class AndroidAudio implements Audio {
 
     SoundPool spool;
@@ -16,8 +18,12 @@ public class AndroidAudio implements Audio {
     String root="Audio/";
     public AndroidAudio(AssetManager assets)
     {
+        AudioAttributes audioAttributes = new AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION).build();
+        this.spool = new SoundPool.Builder().setMaxStreams(20).setAudioAttributes(audioAttributes).build();
+
         this.assets=assets;
-        this.spool= new SoundPool.Builder().setMaxStreams(20).build();
+       // this.spool= new SoundPool.Builder().setAudioAttributes(attributes).setMaxStreams(20).build();
     }
     @Override
     public Sound newSound(String file)
@@ -27,6 +33,7 @@ public class AndroidAudio implements Audio {
         try {
             AssetFileDescriptor descriptor = this.assets.openFd(root+file);
             id=this.spool.load(descriptor,1);
+
         }
         catch(IOException e)
         {
@@ -39,8 +46,7 @@ public class AndroidAudio implements Audio {
     public void playSound(Sound sound)
     {
         AndroidSound s =(AndroidSound)sound;
-        this.spool.play(s.getID(),1,1,1,0,1);
-
+        this.spool.play(s.getID(),1,1,0,0,1);
     }
     @Override
     public void stopSound(Sound sound)
