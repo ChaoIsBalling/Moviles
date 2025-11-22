2a3e2c4a64964dc6f4a178dcfc2c9fbb98040a6f
Chao <114423476+ChaoIsBalling@users.noreply.github.com>
2025-11-22 17:02:11 +0100

    arreglado

--- AndroidEngine/src/main/java/com/example/androidengine/AndroidAudio.java
+++ AndroidEngine/src/main/java/com/example/androidengine/AndroidAudio.java
@@ -52,8 +52,7 @@ public class AndroidAudio implements Audio {
     @Override
     public void loopSound(Sound sound) {
         AndroidSound s =(AndroidSound)sound;
-        this.spool.setLoop(s.getID(),-1);
-        this.spool.play(s.getID(),1,1,0,0,1);
+        this.spool.play(s.getID(),1,1,0,-1,1);
     }
 
     @Override
